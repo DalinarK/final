e224a37aeafad078a4a51ca7ddc5752e43c5173e
Dustin Dinh <djdinh@gmail.com>
2015-06-07 00:09:11 -0700

    fixed bug but still buggy on first try

--- jTinder Touch Slider_files/main.js
+++ jTinder Touch Slider_files/main.js
@@ -6,46 +6,23 @@ function mainTind(usernames) {
 
 	//used to keep track of what user is being liked or disliked in the array that usernames array
 	var slideNum = usernames.length -1;
-
+	var val;
 
 	$("#tinderslide").jTinder({
 		// dislike callback
 	    onDislike: function (item) {
 		    // set the status text
-
-		    var user = usernames['0'];
-		    var hate = usernames[slideNum];
-		    var userInfo = {"username": user, "hate": hate};
 		    slideNum -= 1;
 
-		    		$.post( 
-			"updateHates.php",
-			userInfo,
-			function(info){
-				
-				if (info != null)
-				{	
+    		$("#hateArea").empty();
+			$("#hateArea").append("You currently despise: ");
+			
 
-					var sfas = $.parseJSON(info);
-					console.log(info);
-					console.log("Your hates: " + sfas.hatelist);
-					
-
-					var val = sfas.hatelist;
-					var x;
-					$("#hateArea").empty();
-					$("#hateArea").append("You currently despise: ");
-					
-		
-					for (x in val)
-					{
-						$("#hateArea").append(val[x] + ", ");
-					}
-						
-				}
-							
+			for (x in val)
+			{
+				$("#hateArea").append(val[x] + ", ");
 			}
-		);
+
 	    },
 		// like callback
 	    onLike: function (item) {
@@ -78,7 +55,7 @@ function mainTind(usernames) {
 					console.log("Your hates: " + sfas.hatelist);
 					
 
-					var val = sfas.hatelist;
+					val = sfas.hatelist;
 					var x;
 					$("#hateArea").empty();
 					$("#hateArea").append("You currently despise: ");
@@ -89,7 +66,7 @@ function mainTind(usernames) {
 						$("#hateArea").append(val[x] + ", ");
 					}
 					
-					//prints out all the people user hates so far
+					//prints out all the people user hate so far
 					if (sfas.match == "1")
 					{
 
@@ -108,8 +85,8 @@ function mainTind(usernames) {
 			                        if (userData[count].username == sfas.hate)
 			                        {
 			                        	$("body").empty();
-			                        	$("body").append('<h3 class = "container text-center"> Human, you have found someone who detests your very existence as you they </h3> <br> <h3 class = "container text-center"> To facilitate your mutual hatred, their address is: ' + sfas.StreetNum + ' ' + sfas.Street + ' ' + sfas.City + '. </h3>');
-			                        	$("body").append( '<form class = "container form-signin text-center " id = "myForm" > <input type="submit" class = "btn btn-lg btn-primary btn-block" value = "I want to find more people to hate"/> </form>');
+			                        	$("body").append("<h1> Human, you have found someone who detests your very existence as you they </h1> <br> To facilitate your mutual hatred, we have provided you with their address. It is: " + sfas.StreetNum + " " + sfas.Street + " " + sfas.City + ".");
+			                        	$("body").append( '<form id = "myForm" > <input type="submit" value = "Go Back" id ="submit"/></form>');
 	
 			                        }
 		                					 count += 1;
